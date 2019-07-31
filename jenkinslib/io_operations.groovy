b337e348cabb4d77ea5039016bf1089fe9c37226
admssa <ssa.root@gmail.com>
2019-07-31 16:20:00 +0100

    tedt tag0-5.0-6

--- jenkinslib/io_operations.groovy
+++ jenkinslib/io_operations.groovy
@@ -3,25 +3,25 @@
 import groovy.io.*
 
 @NonCPS
-def listdirs(dir) {
+def listDirs(dir) {
     def currentDir = new File(dir)
     def dirs = []
     currentDir.eachFile FileType.DIRECTORIES, {
         dirs << it.name
-    }
-    return dirs
+}
+return dirs
 }
 
-def getdir(tag, dir){
-    dirs = listdirs(dir)
-    def tag_in_dirs = false    
+def getDir(tag, dir){
+    dirs = listDirs(dir)
+    def tag_in_dirs = false
     double_tag = tag.tokenize("-")[0] + "-" + tag.tokenize("-")[1]
     single_tag = tag.tokenize("-")[0]
     if (double_tag in dirs) {
         println "Got double"
         return double_tag
     }
-    else if(single_tag in dirs) {
+    else if (single_tag in dirs) {
         println "Got single"
         return single_tag
     }
