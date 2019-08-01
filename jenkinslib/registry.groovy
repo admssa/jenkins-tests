fccc4786a8836f98df8f1d55d2db61f3da9c9b89
admssa <ssa.root@gmail.com>
2019-08-01 18:24:36 +0100

    tedt tag0-10.0-8

--- jenkinslib/registry.groovy
+++ jenkinslib/registry.groovy
@@ -1,10 +1,11 @@
 def delete_by_tag(registry, repository, tag){
     def digest = get_digest(registry, repository, tag)
     def result = false
+    def http_client = new URL(url)
     if (digest != null ){
         def url = String.format("http://%s/v2/%s/manifests/%s",registry,repository,digest)
         try {
-            def http_client = new URL(url).openConnection()
+            http_client.openConnection()
             http_client.setRequestMethod('DELETE')
             http_client.setConnectTimeout(5000)
             http_client.setRequestProperty("Accept", "application/vnd.docker.distribution.manifest.v2+json")
@@ -27,8 +28,9 @@ def delete_by_tag(registry, repository, tag){
 def get_digest(registry, repository, tag){
     def url = String.format("http://%s/v2/%s/manifests/%s",registry,repository,tag)
     def responce = null
+    def http_client = new URL(url)
     try {
-        def http_client = new URL(url).openConnection()
+        http_client.openConnection()
         http_client.setRequestMethod('GET')
         http_client.connect()
         http_client.setConnectTimeout(5000)
