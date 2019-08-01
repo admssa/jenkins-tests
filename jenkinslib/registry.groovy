5fd82d9f72a4c951e184823ef2ef0f9380a11893
admssa <ssa.root@gmail.com>
2019-08-01 18:40:52 +0100

    tedt tag0-10.0-11

--- jenkinslib/registry.groovy
+++ jenkinslib/registry.groovy
@@ -1,9 +1,9 @@
 def delete_by_tag(registry, repository, tag){
     def digest = get_digest(registry, repository, tag)
     def result = false
-    def http_client = new URL(url).openConnection()
     if (digest != null ){
         def url = String.format("http://%s/v2/%s/manifests/%s",registry,repository,digest)
+        def http_client = new URL(url).openConnection()
         try {
             http_client.setRequestMethod('DELETE')
             http_client.setConnectTimeout(5000)
@@ -49,7 +49,8 @@ def get_digest(registry, repository, tag){
         if (http_client != null) {
         http_client.disconnect();
         }
-    }   
+    }
+    println "Returning ${responce}"   
     return responce
 }
 
