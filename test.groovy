73bdae8a2a6364341baff8ee871dff1044f30782
admssa <ssa.root@gmail.com>
2019-08-09 18:49:00 +0100

    tedt tag1-001.29

--- test.groovy
+++ test.groovy
@@ -2,24 +2,33 @@
 
 import groovy.json.JsonSlurperClassic;
 
+import net.sf.json.JSONObject;
 
-
-def jsonString = '{"status":"fail","image":"admssa/diag:tag0-001.2","os":{"Negligible":"0(0)","Unknown":"0(0)","Low":"6(6)","Medium":"18(18)","High":"0(0)"},"non-os":{"Negligible":"0(0)","Unknown":"0(0)","Low":"0(0)","Medium":"4(0)","High":"2(0)"}}'
-
-def json = new JsonSlurperClassic().parseText(jsonString)
-
-def formated_message = new String()
-for (object in json){
-   // println object.value.getClass()
-   if (object.value instanceof java.util.HashMap && object.value.size() > 0){
-       object.value = object.value.toString().replaceAll("[^a-zA-Z0-9():]+", " ")
-       print object.value
-   }
-    formated_message = formated_message + "\n*" + object.key.toString() + "*\n" + object.value.toString()
+JSONObject test = new JSONObject()
+if (test.length() != 0){
+   print true
+}
+else {
+   print false
 }
 
+// def jsonString = '{"status":"fail","image":"admssa/diag:tag0-001.2","os":{"Negligible":"0(0)","Unknown":"0(0)","Low":"6(6)","Medium":"18(18)","High":"0(0)"},"non-os":{"Negligible":"0(0)","Unknown":"0(0)","Low":"0(0)","Medium":"4(0)","High":"2(0)"}}'
 
-println formated_message
+// def json = new JsonSlurperClassic().parseText(jsonString)
+
+// def formated_message = new String()
+// for (object in json){
+//    // println object.value.getClass()
+//    if (object.value instanceof java.util.HashMap && object.value.size() > 0){
+//        object.value = object.value.toString().replaceAll("[^a-zA-Z0-9():]+", " ")
+//        print object.value
+//    }
+//     formated_message = formated_message + "\n*" + object.key.toString() + "*\n" + object.value.toString()
+// }
+
+
+// println formated_message
+
 
 
 // for ( e in json ) {
