83d4e95ab45050db5e296ba5cd6e66ef346aa360
admssa <ssa.root@gmail.com>
2019-08-09 18:42:22 +0100

    tedt tag1-001.28

--- jenkinslib/slack.groovy
+++ jenkinslib/slack.groovy
@@ -24,8 +24,7 @@ def jsonToAttachment(short_report, color){
     def msg = new String() 
 
     for (object in short_report){
-        if (object.value != null || object.value.length() > 0) {
-            println object.value
+        if (object.value != null || object.value.length() != 0) {
             if (object.value instanceof net.sf.json.JSONObject && object.value.size() > 0){
                 object.value = object.value.toString().replaceAll("[^a-zA-Z0-9():]+", " ")
             }
