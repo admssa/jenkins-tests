e1d164427cb2c4bed878f249aa8c7b9e28b037c2
admssa <ssa.root@gmail.com>
2019-08-16 17:25:15 +0100

    tedt tag0-all-002

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@ git checkout -b ${branch}
 git add -A
 git commit -m "tedt $1"
 git push --set-upstream origin ${branch}
-git tag $1 -m "---------------------nopush------------------"
+git tag $1 -m "---------------------nopush-------------------"
 git push origin $1
 
 
