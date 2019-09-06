7997702dd3d095391f41427e9c32c636348790df
admssa <ssa.root@gmail.com>
2019-09-06 12:06:16 +0100

    gitn

---
+++ git copy.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+git add -A
+git commit -m "test $1"
+git push --set-upstream origin $(git branch | grep \* | cut -d ' ' -f2)
+git tag $1 -m "---------------------nopush-------------------"
+git push origin $1
+
+
+
